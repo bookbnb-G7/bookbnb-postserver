2ea9a794a67437513ee6cf1a40a7b018a1adedc7
facutorraca <ftorraca@fi.uba.ar>
2020-10-18 22:04:39 -0300

    add check for database connection

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 docker-compose up -d
+docker exec bookbnb-postserver_web bash -c 'while !</dev/tcp/db/5432; do sleep 1; done;'
 docker exec bookbnb-postserver_web pytest --cov=postserver --color=yes
 docker exec bookbnb-postserver_web pylint postserver
 docker-compose down
