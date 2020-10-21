a45d94cfe77c3191268c871443bb17f1f098c399
facutorraca <ftorraca@fi.uba.ar>
2020-10-21 08:57:38 -0300

    add posts tests2

--- postserver/tests/test_posts.py
+++ postserver/tests/test_posts.py
@@ -1,10 +1,12 @@
 import json
-from app.api.crud import post_crud
+from app.api import crud
 
 def test_create_post(test_app, monkeypatch):
-    test_request_payload = {"title": "porque facu es crack", 
-                            "body": " facu es crack xq es facu",
-                            "author": "no soy facu"}
+    test_request_payload = {
+        "title": "porque facu es crack", 
+        "body": " facu es crack xq es facu",
+        "author": "no soy facu"
+    }
     
     test_response_payload = {
         "id": 1,
@@ -20,7 +22,7 @@ def test_create_post(test_app, monkeypatch):
     # This test uses the Pytest monkeypatch fixture to mock out the crud.post
     # function. We then asserted that the endpoint responds with the expected
     # status codes and response body.
-    monkeypatch.setattr(post_crud, "post", mock_post)
+    monkeypatch.setattr(crud.post_crud, "post", mock_post)
 
     response = test_app.post(
         "/posts/",
@@ -30,21 +32,21 @@ def test_create_post(test_app, monkeypatch):
     assert response.status_code == 201
     assert response.json() == test_response_payload
 
-"""
-def test_create_note_invalid_json(test_app):
-    response = test_app.post("/notes/", data=json.dumps({"title": "something"}))
-    assert response.status_code == 422
 
-
-def test_read_note(test_app, monkeypatch):
-    test_data = {"id": 1, "title": "something", "description": "something else"}
+def test_read_post(test_app, monkeypatch):
+    test_data = {
+        "id":1,
+        "title": "porque facu es crack", 
+        "body": " facu es crack xq es facu",
+        "author": "no soy facu"
+    }
 
     async def mock_get(note_id):
         return test_data
 
-    monkeypatch.setattr(crud, "get", mock_get)
+    monkeypatch.setattr(crud.post_crud, "get", mock_get)
 
-    response = test_app.get("/notes/1")
+    response = test_app.get("/posts/1")
     assert response.status_code == 200
     assert response.json() == test_data
 
@@ -53,9 +55,8 @@ def test_read_note_incorrect_id(test_app, monkeypatch):
     async def mock_get(note_id):
         return None
 
-    monkeypatch.setattr(crud, "get", mock_get)
+    monkeypatch.setattr(crud.post_crud, "get", mock_get)
 
     response = test_app.get("/notes/999")
     assert response.status_code == 404
-    assert response.json()["detail"] == "Note not found"
-"""
+    assert response.json()["detail"] == "Not Found"
