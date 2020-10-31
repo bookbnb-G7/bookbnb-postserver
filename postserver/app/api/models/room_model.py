19a5434b21e1d9a349d752abe4a2bbbb39bac7be
facutorraca <ftorraca@fi.uba.ar>
2020-10-31 17:08:00 -0300

    fix - change owner_id from string to integer

--- postserver/app/api/models/room_model.py
+++ postserver/app/api/models/room_model.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 class RoomSchema(BaseModel):
     type: str
     owner: str
-    owner_id: str
+    owner_id: int
     price_per_day: float
 
 class RoomDB(RoomSchema):
