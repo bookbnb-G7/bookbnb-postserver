41a8bfe911708850b241b23cf1131d76686a58d9
nicofandos <nfandos@fi.uba.ar>
2021-01-07 02:57:48 -0300

    fix get single comment

--- postserver/app/api/routes/room_comment_router.py
+++ postserver/app/api/routes/room_comment_router.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from app.db import Session, get_db
-from app.services.auth import auth_service 
+from app.services.auth import auth_service
 from fastapi import APIRouter, Depends, Header
 from app.api.crud.room_comment_dao import RoomCommentDAO
 from app.api.models.room_comment_model import (RoomCommentSchema,
@@ -28,16 +28,24 @@ async def get_all_room_comments(
     auth_service.verify_apy_key(api_key)
     room_comments_list = RoomCommentDAO.get_all_comments(db, room_id)
     amount_comments = len(room_comments_list)
-    return {"room_id": room_id, "amount": amount_comments, "comments": room_comments_list}
+    return {
+        "room_id": room_id,
+        "amount": amount_comments,
+        "comments": room_comments_list
+    }
 
 
 @router.get("/{comment_id}", response_model=RoomCommentDB, status_code=200)
 async def get_room_comment(
-    room_id: int, db: Session = Depends(get_db),
+    room_id: int,
+    comment_id: int,
+    db: Session = Depends(get_db),
     api_key: Optional[str] = Header(None)
 ):
     auth_service.verify_apy_key(api_key)
-    room_comment = RoomCommentDAO.get_comment(db, room_id, comment_id)
+    room_comment = RoomCommentDAO.get_comment(db,
+                                              room_id,
+                                              comment_id)
     return room_comment
 
 
